c8c0587e62b9a62d5881b6b84de53e21e0601328
CaYpeN1 <66198468+CaYpeN1@users.noreply.github.com>
2022-04-25 00:14:43 +0500

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,6 +1,7 @@
 module.exports = async (bot,message,args,argsF) => {
 
-    const user = message.mentions.users.first();   
+    const user = message.mentions.users.first()
+    const colorrole = message.member.displayColor
     const {author} = message;
     const url = await (user?user:author).avatarURL({dynamic: true, size: 512});
     if(!url) return message.reply("У него нет авы!");
@@ -9,6 +10,7 @@ module.exports = async (bot,message,args,argsF) => {
         embeds: [{
             title: `Аватарка хорошего человека`,
             description: `И его зовут ${user?user:author}`,
+            color: colorrole,
             image: {url}
         }]
     });
