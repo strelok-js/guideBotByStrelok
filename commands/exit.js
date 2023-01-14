aa22abf6c6a4319fb080a874256cbbbac855425b
CaypenNow <66198468+CaypenNow@users.noreply.github.com>
2023-01-14 23:08:19 +0500

    Update exit.js

--- commands/exit.js
+++ commands/exit.js
@@ -1,7 +1,6 @@
 module.exports = async (bot,message,args,argsF) => {
 
-    if(message.author.id !== "423499015007502336") return;
-    process.exit();
+    if(message.author.id === "423499015007502336") process.exit();
     
 };
 module.exports.names = ["exit"];
