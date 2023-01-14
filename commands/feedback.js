e7fcb358f891b104e3a7b3531303531b18b7b183
CaypenNow <66198468+CaypenNow@users.noreply.github.com>
2023-01-14 23:09:21 +0500

    Update feedback.js

--- commands/feedback.js
+++ commands/feedback.js
@@ -3,7 +3,8 @@ module.exports = async (bot,message,args,argsF) => {
     if(message.author.id !== "423499015007502336") return;
     const admin = bot.users.cache.get("423499015007502336");
 
-    admin.send(`${message.author.username} пишет: ${message.content}`);
+    admin.send(`${message.author.username} пишет: ${message.content}`)
+    .catch(err => null);
 
 };
 module.exports.names = ["feedback"];
