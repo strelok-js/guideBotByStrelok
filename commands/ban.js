b3c795e06f0e1b7f64d75796e0d0cff48feab5d2
CaypenNow <66198468+CaypenNow@users.noreply.github.com>
2023-01-14 23:06:54 +0500

    update

--- commands/ban.js
+++ commands/ban.js
@@ -1,25 +1,21 @@
 const {Permissions} = require('Discord.js');
 module.exports = async (bot,message,args,argsF) => {
 
-    if(!message.channel.permissionsFor(message.author).has(Permissions.FLAGS.BAN_MEMBERS)) {
-        return message.reply("У тебя не достаточно прав");
-    }
-    const user = message.mentions.users.first();   
+    if(!message.channel.permissionsFor(message.author).has(Permissions.FLAGS.BAN_MEMBERS)) return message.reply("У тебя не достаточно прав");
+    const user = message.mentions.users.first();
     if(!user) return message.reply("Упомяни пользователя!");
     const userMember = message.guild.members.cache.get(user.id);
     
-    if(userMember.bannable) {
-        userMember.ban();
-        return message.reply({
+    if(!userMember.bannable) return message.reply("У тебя не достаточно прав");
+    
+        userMember.ban()
+        message.reply({
             embeds: [{
                 title: "Человек забанен",
-                description: "Этим человеком был " + `${userMember}`,
+                description: `Этим человеком был ${userMember}`,
                 color: "RED"
             }]
-        });
-    } else return message.reply("У тебя не достаточно прав");
-
-    
+        })
     
 };
 module.exports.names = ["ban"];
